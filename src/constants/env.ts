55a460501fc6f00e403756d5472424d8cfcba074
Sithija Shehara <nsithijashehara@gmail.com>
2025-12-05 22:44:39 +0530

    port config updated

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,14 +1,14 @@
-// const getEnv = (
-//   key: string,
-//   defaultValue?: string | number
-// ): string | number => {
-//   const value = process.env[key] || defaultValue;
+const getEnv = (
+  key: string,
+  defaultValue?: string | number
+): string | number => {
+  const value = process.env[key] || defaultValue;
 
-//   if (value === undefined) {
-//     throw Error(`Missing String environment variable for ${key}`);
-//   }
+  if (value === undefined) {
+    throw Error(`Missing String environment variable for ${key}`);
+  }
 
-//   return value;
-// };
+  return value;
+};
 
-// export const PORT = getEnv("VITE_PORT", 5173);
+export const PORT = getEnv("VITE_PORT", 3000);
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import federation from "@originjs/vite-plugin-federation";
+import { PORT } from "./src/constants/env";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,18 +12,18 @@ export default defineConfig({
     federation({
       name: "host_app",
       remotes: {
-        remote_app: "http://localhost:3001/assets/remoteEntry.js",
+        remote_app: `http://localhost:${PORT}/assets/remoteEntry.js`,
       },
       shared: ["react", "react-dom"],
     }),
   ],
   server: {
-    port: 3000, // Your desired port
+    port: Number(PORT), // Your desired port
     strictPort: true, // Exit if port is already in use (optional)
     host: true, // Listen on all addresses (optional)
   },
   preview: {
-    port: 3000,
+    port: Number(PORT),
     strictPort: true,
   },
   build: {
